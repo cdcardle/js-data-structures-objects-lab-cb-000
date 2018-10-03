1a19441b7eeb412034482384dec2898906686891
Chris Cardle <cdcardle@gmail.com>
2018-10-03 14:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const driver = {};
 
-function updateDriverWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, {[key]: value});
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(obj, {[key]: value});
 }
