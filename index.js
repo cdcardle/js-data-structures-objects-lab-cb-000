8a36dae8da5eddf9b85cca138d591ae3b583b099
Chris Cardle <cdcardle@gmail.com>
2018-10-03 14:48:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  delete driver.key;
+  delete driver[key];
   return driver;
 }
 
